5b7fe77a83d9d627fb43822569640ae2b6615d4b
sveng93 <svenvanginkel@icloud.com>
2025-12-01 09:32:38 +0100

    Release V0.0.2

--- config/pluggedin.php
+++ config/pluggedin.php
@@ -6,8 +6,8 @@ return [
     /**
      * General settings.
      */
-    'build_date' => Carbon::parse('2025-10-07'),
+    'build_date' => Carbon::parse('2025-12-01'),
 
-    'build_version' => 'v0.0.1',
+    'build_version' => 'v0.0.2',
 
 ];
